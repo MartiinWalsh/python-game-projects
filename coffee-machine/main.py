3042ce40ef2e95d0c5e7b700499f57014ec75fcb
martinw-dev <martinwalsh.dev@gmail.com>
2022-06-09 16:29:21 +0100

    Coffee machine project added

---
+++ coffee-machine/main.py
@@ -0,0 +1,22 @@
+from menu import Menu, MenuItem
+from coffee_maker import CoffeeMaker
+from money_machine import MoneyMachine
+
+coffee_machine = CoffeeMaker()
+menu = Menu()
+money_machine = MoneyMachine()
+
+coffee_machine_is_on = True
+
+while coffee_machine_is_on:
+    options = menu.get_items()
+    user_choice = input(f"What would you like? ({options}):")
+    if user_choice == "off":
+        coffee_machine_is_on = False
+    elif user_choice == "report":
+        coffee_machine.report()
+        money_machine.report()
+    else:
+        drink = menu.find_drink(user_choice)
+        if coffee_machine.is_resource_sufficient(drink) and money_machine.make_payment(drink.cost):
+            coffee_machine.make_coffee(drink)
