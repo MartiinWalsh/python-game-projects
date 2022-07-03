25bbb999ac989d21220a1a93610e73999fb7ede0
martinw-dev <martinwalsh.dev@gmail.com>
2022-07-03 21:36:26 +0100

    Tail collision added

--- snake-game/main.py
+++ snake-game/main.py
@@ -32,15 +32,24 @@ while is_game_on:
     # Detect collision with food
     if snake.head.distance(food) < 15:
         food.refresh()
+        snake.extend()
         scoreboard.increase_score()
 
+    # Detect collision with wall
     if (
         snake.head.xcor() > 280
         or snake.head.xcor() < -280
         or snake.head.ycor() > 280
         or snake.head.ycor() < -280
     ):
-        pass
+        is_game_on = False
+        scoreboard.game_over()
+
+    # Detect collision with tail
+    for segment in snake.segments[1:]:
+        if snake.head.distance(segment) < 10:
+            is_game_on = False
+            scoreboard.game_over()
 
 
 screen.exitonclick()
