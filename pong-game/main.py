83bc150cb01d06f4d6b76b6e841a3593f6f2ba27
martinw-dev <martinwalsh.dev@gmail.com>
2022-07-06 08:45:55 +0100

    Paddle functionality added

--- pong-game/main.py
+++ pong-game/main.py
@@ -1,9 +1,24 @@
 from turtle import Screen
+from paddle import Paddle
 
 screen = Screen()
 screen.setup(width=800, height=600)
 screen.bgcolor("black")
 screen.title("Pong Game")
+screen.tracer(0)
+
+paddle_r = Paddle((350, 0))
+paddle_l = Paddle((-350, 0))
+
+screen.listen()
+screen.onkey(paddle_r.go_up, "Up")
+screen.onkey(paddle_r.go_down, "Down")
+screen.onkey(paddle_l.go_up, "w")
+screen.onkey(paddle_l.go_down, "s")
+
+is_game_on = True
+while is_game_on:
+    screen.update()
 
 
 screen.exitonclick()
