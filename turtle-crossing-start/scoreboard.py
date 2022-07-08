d755da2c712a886435b2cdd6c4633bc9726679ab
martinw-dev <martinwalsh.dev@gmail.com>
2022-07-08 17:15:47 +0100

    Scoreboard class defined

--- turtle-crossing-start/scoreboard.py
+++ turtle-crossing-start/scoreboard.py
@@ -1,5 +1,23 @@
+from turtle import Turtle
+
 FONT = ("Courier", 24, "normal")
 
 
-class Scoreboard:
-    pass
+class Scoreboard(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.color("black")
+        self.penup()
+        self.hideturtle()
+        self.level = 0
+        self.update_scoreboard()
+
+    def update_scoreboard(self):
+        self.clear()
+        self.level += 1
+        self.goto(-260, 260)
+        self.write(f"Level: {self.level}", align="left", font=FONT)
+
+    def reset(self):
+        self.level = 0
+        self.update_scoreboard()
