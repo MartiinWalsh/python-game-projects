98d4674280758e403ceebdd6b50eb1de6efb130c
martinw-dev <martinwalsh.dev@gmail.com>
2022-07-08 17:50:05 +0100

    Game loop

--- turtle-crossing-start/main.py
+++ turtle-crossing-start/main.py
@@ -7,8 +7,34 @@ from scoreboard import Scoreboard
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.tracer(0)
+screen.title("Turtle Crossing")
 
+player = Player()
+scoreboard = Scoreboard()
+car_manager = CarManager()
+
+screen.listen()
+screen.onkey(player.go_up, "Up")
+
+iter_count = 0
 game_is_on = True
 while game_is_on:
+    iter_count += 1
     time.sleep(0.1)
     screen.update()
+
+    car_manager.create_car()
+    car_manager.move_cars()
+
+    for car in car_manager.all_cars:
+        if player.distance(car) < 20:
+            game_is_on = False
+            scoreboard.game_over()
+
+    if player.is_at_finish_line():
+        scoreboard.update_scoreboard()
+        player.reset()
+        car_manager.level_up()
+
+
+screen.exitonclick()
