c7bbaeccfff0d8ea812d66d261cf9be9c2f8d912
martinw-dev <martinwalsh.dev@gmail.com>
2022-07-08 17:51:59 +0100

    Game Over added

--- turtle-crossing-start/scoreboard.py
+++ turtle-crossing-start/scoreboard.py
@@ -9,15 +9,15 @@ class Scoreboard(Turtle):
         self.color("black")
         self.penup()
         self.hideturtle()
+        self.goto(-260, 260)
         self.level = 0
         self.update_scoreboard()
 
     def update_scoreboard(self):
         self.clear()
         self.level += 1
-        self.goto(-260, 260)
         self.write(f"Level: {self.level}", align="left", font=FONT)
 
-    def reset(self):
-        self.level = 0
-        self.update_scoreboard()
+    def game_over(self):
+        self.goto(0, 0)
+        self.write("GAME OVER", align="center", font=FONT)
