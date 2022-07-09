80a5578d56a844785baa8937c7cabb673b2437a1
martinw-dev <martinwalsh.dev@gmail.com>
2022-07-09 07:23:04 +0100

    Persisting highscore

--- snake-game/scoreboard.py
+++ snake-game/scoreboard.py
@@ -3,6 +3,7 @@ from turtle import Turtle
 
 ALIGNMENT = "center"
 FONT = ("Courier", 24, "normal")
+HIGHSCORES_FILE = "highscores.txt"
 
 
 class ScoreBoard(Turtle):
@@ -10,7 +11,8 @@ class ScoreBoard(Turtle):
         super().__init__()
         self.color("white")
         self.score = 0
-        self.highscore = 0
+        with open(HIGHSCORES_FILE) as file:
+            self.highscore = int(file.read())
         self.penup()
         self.goto(0, 270)
         self.hideturtle()
@@ -27,6 +29,8 @@ class ScoreBoard(Turtle):
     def reset(self):
         if self.score > self.highscore:
             self.highscore = self.score
+            with open(HIGHSCORES_FILE, "w") as file:
+                file.write(f"{self.highscore}")
         self.score = 0
         self.update_scoreboard()
 
